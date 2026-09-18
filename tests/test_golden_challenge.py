28422e4470aa50fe9148ac7a6c02b0e0aed0631c
hippo <135493401+hippoley@users.noreply.github.com>
2026-09-18 11:08:56 +0900

    Fix visible-copy regex in Golden leakage test

--- tests/test_golden_challenge.py
+++ tests/test_golden_challenge.py
@@ -167,8 +167,8 @@ def test_challenge_is_local_only_and_does_not_pollute_research_backend():
 
 
 def test_participant_surface_does_not_name_probe_constructs():
-    visible = re.sub(r"<script[\\s\\S]*?</script>", " ", HTML, flags=re.I)
-    visible = re.sub(r"<style[\\s\\S]*?</style>", " ", visible, flags=re.I)
+    visible = re.sub(r"<script[\s\S]*?</script>", " ", HTML, flags=re.I)
+    visible = re.sub(r"<style[\s\S]*?</style>", " ", visible, flags=re.I)
     visible = re.sub(r"<[^>]+>", " ", visible)
     for forbidden in [
         "PF01",
